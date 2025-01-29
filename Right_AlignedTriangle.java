230e8456d836230500d464bf6e7aad67c0e838ae
rahul30824 <rahulr684.sse@saveetha.com>
2025-01-29 20:48:26 +0530

    Right_AlignedTriangle.java

---
+++ Right_AlignedTriangle.java
@@ -0,0 +1,21 @@
+class Main{
+    public static void main(String[] args)
+    {
+      int rows=5;
+      for(int i=0;i<=rows;i++)
+      {
+          for(int j=1;j<=rows;j++)
+          {
+              if(i+j<=rows)
+              {
+                  System.out.print(" ");
+              }
+              else{
+                  System.out.print("*");
+              }
+             
+          }
+           System.out.println();
+      }
+    }
+}
