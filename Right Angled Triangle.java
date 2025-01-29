0b9a05bf952b3143f497e94a2ec4dd6bd303bda0
rahul30824 <rahulr684.sse@saveetha.com>
2025-01-29 22:55:18 +0530

    Update Right Angled Triangle.java

--- Right Angled Triangle.java
+++ Right Angled Triangle.java
@@ -2,7 +2,9 @@ class Main{
     public static void main(String[] args)
     {
     int n=5;
+    
     for(int i=0;i<=n;i++)
+        //for every i j must fulfill his print remember this//
     {
     for(int j=0;j<=i;j++)
         {
